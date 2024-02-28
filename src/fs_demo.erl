f74b96b74e568f758a3b1530f86a89f0205ce991
Duncan McGreggor <oubiwann@gmail.com>
2024-02-27 20:39:34 -0600

    Moved demo code into own file.

---
+++ src/fs_demo.erl
@@ -0,0 +1,25 @@
+-module(fs_demo).
+
+-include_lib("fs/include/const.hrl").
+
+-export([start_looper/0,
+         start_looper/1]).
+
+% sample subscriber
+
+start_looper() -> start_looper(?DEFAULT_NAME).
+
+start_looper(Name) ->
+    spawn(fun () ->
+                  subscribe(Name),
+                  loop()
+          end).
+
+loop() ->
+    receive
+        {_Pid, {fs, file_event}, {Path, Flags}} ->
+            error_logger:info_msg("file_event: ~p ~p",
+                                  [Path, Flags]);
+        _ -> ignore
+    end,
+    loop().
